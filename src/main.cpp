01c5abef5adaba499a0b2d64d89fd9e905fd731c
Yestin Paraguya <yestinjohn.paraguya@g.msuiit.edu.ph>
2026-09-18 16:31:02 +0800

    Initialize ESP32 PlatformIO project

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "esp_log.h"
+
+static const char *TAG = "ROOM_MONITOR";
+
+extern "C" void app_main(void)
+{
+    ESP_LOGI(TAG, "BCA152 FreeRTOS Multisensor");
+    ESP_LOGI(TAG, "System starting...");
+}
